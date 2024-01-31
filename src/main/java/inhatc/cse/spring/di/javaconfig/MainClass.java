710e53369369c4bdc552bcb3f8b71dfbd6e05b71
Kitae's Github <aqua0405@gmail.com>
2024-01-31 18:03:44 +0900

    JavaBean XMLBean

---
+++ src/main/java/inhatc/cse/spring/di/javaconfig/MainClass.java
@@ -0,0 +1,26 @@
+package inhatc.cse.spring.di.javaconfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        Student student1 = ctx.getBean("student1", Student.class);
+        System.out.println(student1.getName());
+        System.out.println(student1.getAge());
+        System.out.println(student1.getHobbys());
+        System.out.println(student1.getHeight());
+        System.out.println(student1.getWeight());
+
+        Student student2 = ctx.getBean("student2", Student.class);
+        System.out.println(student2.getName());
+        System.out.println(student2.getAge());
+        System.out.println(student2.getHobbys());
+        System.out.println(student2.getHeight());
+        System.out.println(student2.getWeight());
+
+        ctx.close();
+    }
+}
